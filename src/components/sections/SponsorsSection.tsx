0f4d07b35ffca53b4e47a99b30803249c08ad3bd
busa8908 <busa8908@gmail.com>
2025-12-07 18:27:37 -0500

    Added sponsors

--- src/components/sections/SponsorsSection.tsx
+++ src/components/sections/SponsorsSection.tsx
@@ -1,22 +1,23 @@
+import '../../styles/global.css';
 
 const TemplateSection = () => {
-  
-  
-  
   return (
     <div className="snap-start w-screen h-screen">
       <div 
-        className="relative w-full h-full overflow-hidden"
+        className="relative w-full h-full overflow-hidden flex items-center justify-center"
         style={{
           backgroundImage: "url('/assets/Sponsors.svg')",
           backgroundPosition: 'center center',
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
         }}
-      />
+      >
+        <h1 className="brim-narrow-text">
+          ANNOUNCING SPONSORS SOON!
+        </h1>
+      </div>
     </div>
   );
-  
 };
 
 export default TemplateSection;
