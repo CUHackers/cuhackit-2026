fc1f3ba713c6c253ab63e767ef2bd099687100bd
hazel <ctorek@clemson.edu>
2026-08-04 09:32:14 -0400

    change base in react router for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cuhackit-2026">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
